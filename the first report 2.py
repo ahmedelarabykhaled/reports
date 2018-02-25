0052838ceaf09432efec9ee2bf06a836f247c9b8
ahmedelarabykhaled <36570211+ahmedelarabykhaled@users.noreply.github.com>
2018-02-25 18:54:53 +0200

    Add files via upload

---
+++ the first report 2.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+img=cv2.imread("images1.jpg")
+img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# lower mask (0-10)
+lower_red = np.array([0,50,50])
+upper_red = np.array([10,255,255])
+mask0 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+# upper mask (170-180)
+lower_red = np.array([170,50,50])
+upper_red = np.array([180,255,255])
+mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+
+# join my masks
+mask = mask0+mask1
+
+# set my output img to zero everywhere except my mask
+output_img = img.copy()
+output_img[np.where(mask==0)] = 0
+
+while (1):
+
+    # or your HSV image, which I *believe* is what you want
+    output_hsv = img_hsv.copy()
+    output_hsv[np.where(mask == 0)] = 0
+    cv2.imshow('1', mask)
+    # cv2.imshow('2', mask0)
+    # cv2.imshow('3', mask1)
+    if cv2.waitKey(1) == 27:
+        break
+cv2.destroyAllWindows()
